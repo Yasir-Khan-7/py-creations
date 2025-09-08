bf5753116dfcb34143914b484375bb03a1d84f95
Yasir Khan <110191648+Yasir-Khan-7@users.noreply.github.com>
2025-09-08 08:10:41 +0500

    started numpy library

--- numpy-library.py
+++ numpy-library.py
@@ -1,1 +1,6 @@
 # IT consist of all the numpy related stuff 
+# why numpy? instead of list ? Reasion:
+# 1. Numpy is faster than list
+# 2. Numpy is more memory efficient than list
+
+import numpy as np 
